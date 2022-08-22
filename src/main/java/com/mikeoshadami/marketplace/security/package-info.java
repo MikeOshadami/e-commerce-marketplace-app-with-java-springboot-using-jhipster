1ea4cc7628933a3ca7746142108ea03e0486832c
Mike Oshadami <oshadami.mj@gmail.com>
2022-08-22 13:57:05 +0100

    Initialize app with jhipster
    Initialize app with jhipster to install all required jhipster modules.

---
+++ src/main/java/com/mikeoshadami/marketplace/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.mikeoshadami.marketplace.security;
---
+++ src/main/java/com/mikeoshadami/marketplace/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.mikeoshadami.marketplace.service.dto;
